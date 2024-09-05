328b9effe015efede3894fcd2e048d318c2eee0b
jonas <114328197+07jh@users.noreply.github.com>
2024-09-06 00:26:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 const t = document.getElementById("t");
-const b = document.getElementById("b");
-const f = document.getElementById("f");
 const fc = document.getElementById("fc");
 let note = 'Write a note here';
 let c = 'empty';
@@ -9,36 +7,18 @@ let fb = 'empty';
 
 initPage();
 
-function setC(n, v) {
-    document.cookie = `${n}=${v}; expires=Mon, 1 January 2050 12:00:00 UTC; path=/`;
-}
-
-function deleteC(n) {
-    document.cookie = `${n}=none; expires=Mon, 1 January 1990 12:00:00 UTC; path=/`;
-}
-
-function getC(n) {
-    c = decodeURIComponent(document.cookie);
-    c = c.slice(c.indexOf('=') + 1);
-    return c;
-}
-
 function initPage() {
-    note = getC('note');
 }
 
-function save() {
-    cb = t.value;
-    setC('note', `${cb}`);
-    f.textContent = f.textContent + '   successfully saved   ';
+function clearcontent() {
+    t.value = '';
 }
 
-function importieren() {
+function importfile() {
     fc.click();
-    f.textContent = f.textContent + '   successfully imported   ';
 }
 
-function exportieren() {
+function exportfile() {
     const text = t.value;
     const blob = new Blob([text], { type: 'text/plain' });
     const link = document.createElement('a');
@@ -46,7 +26,6 @@ function exportieren() {
     link.download = 'simple.txt';
     link.click();
     URL.revokeObjectURL(link.href);
-    f.textContent = f.textContent + '   successfully exported   ';
 }
 
 fc.addEventListener('change', (event) => {
